1a5798284af298d9d7651a0d5c50f06c6566a382
Vanessa McHale <vamchale@gmail.com>
2024-10-29 12:08:23 -0400

    plot out something more rigorous

--- src/Jacinda/Backend/Printf.hs
+++ src/Jacinda/Backend/Printf.hs
@@ -19,25 +19,33 @@ sprintf fmt e = encodeUtf8 (sprintf' (decodeUtf8 fmt) e)
 pf :: Double -> T.Text
 pf = decodeUtf8 . BSL.toStrict . toLazyByteString . doubleDec
 
--- TODO: interpret precision, like %0.6f %.6
+data PF = D !(Maybe Int) | I | B | S
 
--- FIXME: if next is, say %i and encounter an int in the tuple, that should be an error
+{-# SCC next #-}
+next :: T.Text -> (T.Text, Maybe (PF, T.Text))
+next inp = case T.break (=='%') inp of
+    (p, mfmt) | "%i" `T.isPrefixOf` mfmt -> (p, Just (I, T.drop 2 mfmt))
+              | "%s" `T.isPrefixOf` mfmt -> (p, Just (S, T.drop 2 mfmt))
+              | "%b" `T.isPrefixOf` mfmt -> (p, Just (B, T.drop 2 mfmt))
+              | "%f." `T.isPrefixOf` mfmt -> (p, Just (D (Just undefined), undefined))
+              | "%f" `T.isPrefixOf` mfmt -> (p, Just (D Nothing, T.drop 2 mfmt))
+              | otherwise -> (p, Nothing)
+
+{-# SCC sp #-}
+sp :: T.Text -> [E a] -> T.Text
+sp t [] = t
+sp t (e:es) =
+    case (next t, e) of
+        ((_, Nothing), _) -> error "Argument to sprintf has more values than expected by format string."
+        ((p, Just ((D Nothing), fmt)), Lit _ (FLit f)) -> p <> pf f <> sp fmt es
+        ((p, Just (B, fmt)), Lit _ (BLit b)) -> p <> showBool b <> sp fmt es
+        ((p, Just (I, fmt)), Lit _ (ILit i)) -> p <> T.pack (show i) <> sp fmt es
+        ((p, Just (S, fmt)), Lit _ (StrLit bs)) -> p <> decodeUtf8 bs <> sp fmt es
+        ((_, Just{}), _) -> error "Argument type does not match sprintf format string."
+
+  where
+    showBool True = "#t"; showBool False = "#f"
+
 sprintf' :: T.Text -> E a -> T.Text
-sprintf' fmt (Lit _ (FLit f)) =
-    let (prefix, fmt') = T.breakOn "%f" fmt
-        in prefix <> pf f <> T.drop 2 fmt'
-sprintf' fmt (Lit _ (ILit i)) =
-    let (prefix, fmt') = T.breakOn "%i" fmt
-        in prefix <> T.pack (show i) <> T.drop 2 fmt'
-sprintf' fmt (Lit _ (StrLit bs)) =
-    let (prefix, fmt') = T.breakOn "%s" fmt
-        in prefix <> decodeUtf8 bs <> T.drop 2 fmt'
-sprintf' fmt (Tup _ [e]) = sprintf' fmt e
-sprintf' fmt (Tup l (e:es)) =
-    let nextFmt = sprintf' fmt e
-        in sprintf' nextFmt (Tup l es)
-sprintf' fmt (Lit _ (BLit b)) =
-    let (prefix, fmt') = T.breakOn "%b" fmt
-        in prefix <> showBool b <> T.drop 2 fmt'
-    where showBool True  = "true"
-          showBool False = "false"
+sprintf' fmt e@Lit{} = sp fmt [e]
+sprintf' fmt (Tup _ es) = sp fmt es
